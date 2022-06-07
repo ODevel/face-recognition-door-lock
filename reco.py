5e889c0c327b708142b1f6678b6c7b9da8669adf
Sumail S. Brar <brars30@mcmaster.ca>
2022-06-07 10:11:04 -0400

    Adding mqtt

--- reco.py
+++ reco.py
@@ -3,6 +3,75 @@ import numpy as np
 import os
 import time
 
+from pubnub.pubnub import PubNub, SubscribeListener, SubscribeCallback, PNStatusCategory
+from pubnub.pnconfiguration import PNConfiguration 
+from pubnub.exceptions import PubNubException 
+import pubnub
+#import RPi.GPIO as GPIO
+
+pnconf = PNConfiguration()
+pnconf.publish_key = 'pub-c-90b929ee-3089-41e3-b543-586d409ea538'
+pnconf.subscribe_key = 'sub-c-39a4fa00-9103-4fba-867e-4614ebf67e42' 
+pnconf.uuid = "fwefewf12"
+pubnub = PubNub(pnconf)
+channel = 'CP-IoT'
+
+def my_publish_callback(envelope, status):
+    # Check whether request successfully completed or not
+    if not status.is_error():
+        pass  # Message successfully published to specified channel.
+    else:
+        print('Error while pushlishing to MQTT')
+        pass  # Handle message publish error. Check 'category' property to find out possible issue
+        # because of which request did fail.
+        # Request can be resent using: [status retry];
+
+class MySubscribeCallback(SubscribeCallback):
+    def presence(self, pubnub, presence):
+        pass  # handle incoming presence data
+
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            pass  # This event happens when radio / connectivity is lost
+
+        elif status.category == PNStatusCategory.PNConnectedCategory:
+            # Connect event. You can do stuff like publish, and know you'll get it.
+            # Or just use the connected event to confirm you are subscribed for
+            # UI / internal notifications, etc
+            pubnub.publish().channel('CP-IoT').message('Welcome to CP-IoT MQTT ').pn_async(my_publish_callback)
+        elif status.category == PNStatusCategory.PNReconnectedCategory:
+            pass
+            # Happens as part of our regular operation. This event happens when
+            # radio / connectivity is lost, then regained.
+        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
+            pass
+            # Handle message decryption error. Probably client configured to
+            # encrypt messages and on live data feed it received plain text.
+
+    def message(self, pubnub, message):
+        # Handle new message stored in message.message
+        print(message.message)
+
+pubnub.add_listener(MySubscribeCallback())
+pubnub.subscribe().channels('CP-IoT').execute()
+
+def mqtt_send(status) :
+    global pubnub
+    pubnub.publish().channel("CP-IoT").message({
+        'status': status
+    }).pn_async(my_publish_callback)
+ 
+#my_listener = SubscribeListener()                           
+#pubnub.add_listener(my_listener)                              
+#pubnub.subscribe().channels(channel).execute()               
+ 
+#my_listener.wait_for_connect()                               
+print('connected')                                           
+  
+#pubnub.publish().channel(channel).message(data).sync()      
+
+# pub-c-90b929ee-3089-41e3-b543-586d409ea538
+# sub-c-39a4fa00-9103-4fba-867e-4614ebf67e42
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
@@ -81,5 +150,7 @@ for id in id_list:
 
 if(count_correct >= 10):
     print(' ===== ALLOWED to ENTER ===== ')
+    mqtt_send('success to enter')
 else:
     print(' ===== NOT Allowed ===== ')
+    mqtt_send('entry denied')
